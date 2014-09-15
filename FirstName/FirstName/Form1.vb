751155725a6921956faad16ce9280ed669d84945
idsc3101 <idsc3101@gmail.com>
2014-09-15 17:05:57 -0500

    Solved the problem.

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,9 +3,19 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
-        ' Please write your code here
+        Dim FN As String
+        FN = txtFullName.Text
 
-        txtFirstName.Text = "Not implemented yet"
+        Dim FN2 As String
+        FN2 = FN.Trim()
+
+        Dim FirstSpace As Integer
+        FirstSpace = FN2.IndexOf(" ")
+
+        Dim FirstName As String
+        FirstName = FN2.Substring(0, FirstSpace)
+
+        txtFirstName.Text = FirstName
 
     End Sub
 End Class
